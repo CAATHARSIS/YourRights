ab0ce30f09565af557f8768a5941cd855b1ae2fd
BRAGO <vanay2015ooo@yandex.ru>
2021-11-29 17:46:58 +0300

    alpha

--- js/scripts.js
+++ js/scripts.js
@@ -2,10 +2,5 @@ $('.owl-carousel').owlCarousel({
     center: true,
     items: 2,
     loop: true,
-    margin: 10,
-    responsive: {
-        600: {
-            items: 4
-        }
-    }
+    margin: 10
 });
